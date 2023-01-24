e54dda0fb9ebcd5e658d2d388037b4577959685f
RIvanCF <flores.ivan940214@gmail.com>
2023-01-24 15:23:26 -0600

    Adquirimos los datos de la poke api (Aun por mejorar el codigo js)

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+
+for(let i = 1; i <= 150; i++){
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+    fetch(url)
+        .then(Response => Response.json())
+        .then(pokemon => {
+            console.log(pokemon)
+        })
+}
